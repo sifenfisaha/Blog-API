73ce08919322a9307d8354b217bb466ee44acda3
sifanfiseha <sifen.fiseha@astu.edu.et>
2025-08-30 11:26:01 +0300

    feat: create User model with first_name, last_name, email, password

---
+++ src/models/user.model.ts
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+import { Iuser } from "../types/types";
+
+const UserSchema = new mongoose.Schema<Iuser>(
+  {
+    first_name: { type: String, required: true },
+    last_name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+  },
+  { timestamps: true }
+);
