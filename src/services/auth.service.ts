e7c85df74168c35a14e4df2177b37907be4a328f
sifen fisaha <sifenfisaha@gmail.com>
2025-09-29 23:22:16 +0300

    feat(auth): include user ID in login response for frontend use

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -41,6 +41,7 @@ export class AuthService {
 
     const token = TokenService.generateToken({ userId });
     const safeUser = {
+      id: user._id,
       first_name: user.first_name,
       last_name: user.last_name,
       email: user.email,
